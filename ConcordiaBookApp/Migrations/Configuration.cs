568d13bacd21342cec0dbe005a8f1a9964481f41
Brad Erickson <berickson324@gmail.com>
2017-09-21 08:40:41 -0500

    enable automatic migrations

---
+++ ConcordiaBookApp/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ConcordiaBookApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ConcordiaBookApp.Models.BookStoreDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "ConcordiaBookApp.Models.BookStoreDBContext";
+        }
+
+        protected override void Seed(ConcordiaBookApp.Models.BookStoreDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
